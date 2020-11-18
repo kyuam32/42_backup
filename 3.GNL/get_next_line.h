4a6aee27a490621917b8401bdcc4aa1019b0e184
lnk0302 <lnk0302@users.noreply.github.com>
2020-11-18 10:42:23 +0900

    20201118

---
+++ 3.GNL/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlee <nlee@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/16 20:03:21 by nlee              #+#    #+#             */
+/*   Updated: 2020/11/16 20:14:28 by nlee             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int get_next_line(int fd, char **line);
+
+#endif
